d022f4f39e236832fd45b788678c34d02af0086e
Mickael <mickael.bastard.68@gmail.com>
2022-03-19 20:41:23 +0100

    Update utils_plots.py
    Updated palette of colors. Implemented  barPlotGrid() and barPlotGrid_values() functions for the map or bar plots.

--- utils/utils_plots.py
+++ utils/utils_plots.py
@@ -7,9 +7,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.decomposition import PCA
+import math as math
 
 # Set palette for colors
-mypalette = ["#008248", "#604c4c", "#eac784", "#f0cddb", "#6B9997"]
+#mypalette = ["#008248", "#604c4c", "#eac784", "#f0cddb", "#6B9997"]#Old
+mypalette = ["#0b84a5", "#f6c85f", "#6f4e7c", "#9dd866", "#ca472f", "#ffa056","#8dddd0"]
 mypalette_d = ["#cce6da", "#66b491", "#008248"]
 genders = ["#8700f9", "#00c4a9", "#4462D1"]
 
@@ -54,17 +56,110 @@ def customersByClusters(profile, kmeans_clusters, filename=""):
     fig = ax.get_figure()
     if filename != "":
         fig.savefig(filename+".png") 
-    
+
 # Function plotting a collection of informative variable subplots for final interpretation of the different clusters.
 # Each subplot shows how one variable is distributed across clusters.
-def barPlotGrid(profile, kmeans_clusters, filename=""):
-    df = profile.copy()
+def barPlotGrid(profile, kmeans_clusters, filename="", opt_col_order=[]):
+      
+    df = profile.copy()    
+    
+    if len(opt_col_order)==0:
+        opt_col_order = list(df.columns)
+        opt_col_order.remove("id_membership")
+    
+    
+    #Remove prep_ in column names
+    for ind, column in enumerate(df):
+        if 'prep_' in column:
+            new_col = column.replace("prep_", "", 1)
+            df.rename(columns = {column:new_col}, inplace = True)
+            # find the index
+            i = opt_col_order.index(column)
+            opt_col_order = opt_col_order[:i]+[new_col]+opt_col_order[i+1:]
+    
+    #sns.set_theme(style="darkgrid")
+    sns.set_style("whitegrid")
     df['cluster'] = kmeans_clusters
     df = df.melt(id_vars=['id_membership', 'cluster'])
     df = df.query('cluster != -1').groupby(['cluster', 'variable']).mean().reset_index()
     
-    g = sns.FacetGrid(df, col='variable', hue='cluster', col_wrap=5, height=2, sharey=False, palette=mypalette)
+    
+    g = sns.FacetGrid(df, col='variable', hue='cluster', col_wrap=3, height=2, sharey=False, palette=mypalette, 
+                      col_order=opt_col_order)
     g = g.map(plt.bar, 'cluster', 'value').set_titles("{col_name}")
     if filename != "":
         g.savefig(filename+".png")  
+
+        
+                
+# bar plot function printing values 
+def f(cluster,value, **kwargs):
+    ax = plt.bar(x=cluster,height=value,**kwargs)
     
+    #plt.xticks(list(range(len(cluster))), list(range(len(cluster))))
+    #l = cluster.shape[0]
+    #plt.xticks(list(range(l)))
+    
+    for i in range(len(cluster)):
+        plt.annotate(str(round(value.values[i],2)), xy=(cluster.values[i],0), ha='center', va='bottom',rotation=90)
+        #plt.annotate(str(round(value.values[i],2)), xy=(cluster.values[i],value.values[i]), ha='center', va='bottom')
+    
+    
+# Function plotting a collection of informative variable subplots for final interpretation of the different clusters.
+# Each subplot shows how one variable is distributed across clusters.
+def barPlotGrid_values(profile, kmeans_clusters, filename="", opt_col_order=[], sharey_lst=[]):
+    
+    df = profile.copy() 
+      
+    if len(opt_col_order)==0:
+        opt_col_order = list(df.columns)
+        opt_col_order.remove("id_membership")
+    
+    
+    #Remove prep_ in column names
+    for ind, column in enumerate(df):
+        if 'prep_' in column:
+            new_col = column.replace("prep_", "", 1)
+            df.rename(columns = {column:new_col}, inplace = True)
+            # find the index
+            if column in opt_col_order:
+                i = opt_col_order.index(column)
+                opt_col_order = opt_col_order[:i]+[new_col]+opt_col_order[i+1:]    
+        
+           
+    df['cluster'] = kmeans_clusters
+    df = df.melt(id_vars=['id_membership', 'cluster'])
+    df = df.query('cluster != -1').groupby(['cluster', 'variable']).mean().reset_index()
+    
+    nb_clust = len(df["cluster"].unique())
+        
+    
+    #l=l
+    #plt.subplots(1, l)
+    
+    
+    col_wrap=3
+    l = math.ceil(len(opt_col_order)/col_wrap)
+    j=0
+    k=col_wrap
+    
+    #if len(sharey_lst)!=l:
+    #    [True]*l
+           
+    for i in range(l):
+        #plt.subplot(1, l, i+1)
+    
+        g = sns.FacetGrid(df, col='variable', hue='cluster', col_wrap=col_wrap, height=2, sharey=sharey_lst[i], palette=mypalette, 
+                      col_order=opt_col_order[j:k])
+        #g = g.map(plt.bar, 'cluster', 'value').set_titles("{col_name}")
+        g = g.map(f, 'cluster', 'value').set_titles("{col_name}")
+        
+        plt.xticks(list(range(nb_clust)))
+        
+        j=j+col_wrap
+        k=k+col_wrap
+        
+        if filename != "":
+            g.savefig(filename + "_" + str(i+1) + ".png")
+            
+    return df
