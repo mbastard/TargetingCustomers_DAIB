07acd14c644b825e48a1835b364e567c9c541755
Mickael <mickael.bastard.68@gmail.com>
2022-03-21 15:16:13 +0100

    Update utils_plots.py
    Implemented screePlot_2axis() and implemented 200 dpi option for saving plots

--- utils/utils_plots.py
+++ utils/utils_plots.py
@@ -42,9 +42,65 @@ def screePlot(profile, var_explained=0.8):
     ax2 = sns.lineplot(ind, cumvals, ci=None, color="black")
     ax.grid(b=True, which='major', linewidth=0.5)
 
-    ax.set_xlabel("Principal component")
-    ax.set_ylabel("Variance explained (%)")
-    plt.title('Explained variance per principal component');
+    ax.set_xlabel("Component number")
+    ax.set_ylabel("Percentage of explained variance")
+    plt.title('Scree plot of explained variance');
+    
+# Function generating scree-plot
+def screePlot_2axis(profile, var_explained=0.8, filename=""):
+    
+    profile = profile.set_index("id_membership") # set id_membership as index
+    
+    # Apply feature scaling - leaving variances unequal is equivalent to putting more weight on variables with smaller variance
+    scaler = StandardScaler().fit(profile)
+    profile_scaled = scaler.transform(profile)
+    
+    # Apply dimensionality reduction -  k-means algorithm is both more effective and more efficient with a small number of dimensions
+    # Use PCA to reduce dimensionality
+    
+    pca = PCA()
+    X_pca = pca.fit_transform(profile_scaled)
+    
+    # Scree plot (variance explained by each principal component)
+    num_components=len(pca.explained_variance_ratio_)
+    ind = np.arange(num_components)
+    vals = pca.explained_variance_ratio_
+    cumvals = np.cumsum(vals)
+        
+    fig, ax1 = plt.subplots(figsize=(10,6))    
+    
+    color = 'tab:blue'
+    ax1.set_xlabel('Component number')
+    ax1.set_ylabel('Explained variance per component (%)', color=color)
+    ax1.bar(ind, vals, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax1.grid(False)
+    
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+    color = 'tab:green'
+    
+    
+    ax2.set_ylabel('Cumulated explained variance (%)', color=color)  # we already handled the x-label with ax1
+    ax2.plot(ind, cumvals, color=color)
+    ax2.scatter(ind, cumvals, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+    
+    ax1.set_ylim(0,0.4)
+    ax2.set_ylim(0,1.2)
+    
+    
+    for i, txt in enumerate(cumvals):
+        ax2.annotate(str(int(round(cumvals[i],2)*100)) + "%", (ind[i], cumvals[i]+0.05), rotation=90, color=color)
+    
+    
+    
+    plt.title('Scree plot of explained variance');
+    
+    if filename != "":
+        fig.savefig(filename+".png", dpi=200) 
+
+    
     
 
 # Function plotting the number of customers by clusters
@@ -53,9 +109,14 @@ def customersByClusters(profile, kmeans_clusters, filename=""):
     df['cluster'] = kmeans_clusters
     ax = sns.countplot(x='cluster', data=df, palette=mypalette)
     ax.set_title('Customers by cluster');
+    
+    for p in ax.patches:
+        ax.annotate(p.get_height(), (p.get_x()+0.3, p.get_height()+50))
+    
+    
     fig = ax.get_figure()
     if filename != "":
-        fig.savefig(filename+".png") 
+        fig.savefig(filename+".png", dpi=200) 
 
 # Function plotting a collection of informative variable subplots for final interpretation of the different clusters.
 # Each subplot shows how one variable is distributed across clusters.
@@ -103,11 +164,12 @@ def f(cluster,value, **kwargs):
     for i in range(len(cluster)):
         plt.annotate(str(round(value.values[i],2)), xy=(cluster.values[i],0), ha='center', va='bottom',rotation=90)
         #plt.annotate(str(round(value.values[i],2)), xy=(cluster.values[i],value.values[i]), ha='center', va='bottom')
+        #plt.ylabel("test")
     
     
 # Function plotting a collection of informative variable subplots for final interpretation of the different clusters.
 # Each subplot shows how one variable is distributed across clusters.
-def barPlotGrid_values(profile, kmeans_clusters, filename="", opt_col_order=[], sharey_lst=[]):
+def barPlotGrid_values(profile, kmeans_clusters, filename="", opt_col_order=[], sharey_lst=[],ylabel_lst=[]):
     
     df = profile.copy() 
       
@@ -148,18 +210,22 @@ def barPlotGrid_values(profile, kmeans_clusters, filename="", opt_col_order=[], 
            
     for i in range(l):
         #plt.subplot(1, l, i+1)
-    
+            
         g = sns.FacetGrid(df, col='variable', hue='cluster', col_wrap=col_wrap, height=2, sharey=sharey_lst[i], palette=mypalette, 
                       col_order=opt_col_order[j:k])
         #g = g.map(plt.bar, 'cluster', 'value').set_titles("{col_name}")
         g = g.map(f, 'cluster', 'value').set_titles("{col_name}")
         
+        if len(ylabel_lst) != 0:
+            g.set(ylabel=ylabel_lst[i])
+        
         plt.xticks(list(range(nb_clust)))
+        
         
         j=j+col_wrap
         k=k+col_wrap
         
         if filename != "":
-            g.savefig(filename + "_" + str(i+1) + ".png")
+            g.savefig(filename + "_" + str(i+1) + ".png", dpi=200)
             
     return df
